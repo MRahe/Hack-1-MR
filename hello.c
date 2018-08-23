1c731a0999ae15d6e9d2c6453ee71a4dc11d99f6
MRahe <42557390+MRahe@users.noreply.github.com>
2018-08-23 14:14:16 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Michael Rahe, Michael Brandt
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Michael and Michael!\n");
+
+  return 0;
+}
